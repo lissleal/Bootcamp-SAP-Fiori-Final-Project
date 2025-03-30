b1ead8cdb60291c1c422c27657c47764d7da87f4
Lissett Leal Naranjo <lissett777@gmail.com>
2025-03-30 16:59:40 +0000

    Agrega vista detail con detalle de proveedores y smarttable de productos

--- webapp/controller/Detail.controller.js
+++ webapp/controller/Detail.controller.js
@@ -1,12 +1,24 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
     "com/bootcamp/sapui5/providers/utils/HomeHelper"
-   ], (Controller, HomeHelper) => {
+], (Controller, HomeHelper) => {
     "use strict";
     return Controller.extend("com.bootcamp.sapui5.providers.controller.Detail",
-   {
-    onInit() {
-    }
-    });
-   });
-   
+        {
+            onInit() {
+                let oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+
+                oRouter.getRoute("detail").attachPatternMatched(this._onObjectMatched, this);
+            },
+            _onObjectMatched: function (oEvent) {
+                // Obtener el ProductID de la URL y enlazar el contexto
+                let sSupplierID = oEvent.getParameter("arguments").SupplierID;
+                this.getView().bindElement({
+                    path: "/Suppliers(" + sSupplierID + ")",
+                    parameters: {
+                        expand: "Products"
+                    }
+                });
+            },
+        });
+});
