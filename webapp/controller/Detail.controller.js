4c7d0d1332ff1f1f3a5064873144ff033ec6f461
Lissett Leal Naranjo <lissett777@gmail.com>
2025-03-30 16:24:03 +0000

    Agrega vista detail y navegacion a ella

---
+++ webapp/controller/Detail.controller.js
@@ -0,0 +1,12 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller",
+    "com/bootcamp/sapui5/providers/utils/HomeHelper"
+   ], (Controller, HomeHelper) => {
+    "use strict";
+    return Controller.extend("com.bootcamp.sapui5.providers.controller.Detail",
+   {
+    onInit() {
+    }
+    });
+   });
+   
--- webapp/controller/Home.controller.js
+++ webapp/controller/Home.controller.js
@@ -6,11 +6,21 @@ sap.ui.define([
 
     return Controller.extend("com.bootcamp.sapui5.providers.controller.Home", {
         onInit() {
+            this.oRouter = this.getOwnerComponent().getRouter();
         },
 
         onPress: async function(){
             let oDatos = await HomeHelper.getDataProviders();
             await HomeHelper.setProviderModel(this, oDatos[0].results);
-        }
+        },
+
+        onItemPress: function(oEvent){
+            let oSource = oEvent.getSource();
+            let oDatos = oSource.getBindingContext("ProviderCollection").getObject();
+            this.oRouter.navTo("detail", {
+            SupplierID: oDatos.SupplierID
+            });
+            }
+            
     });
 });
