3f9f695035d2959c4f0ce3a1b56428e668b81214
Lissett Leal Naranjo <lissett777@gmail.com>
2025-03-31 01:32:21 +0000

    Agrega dialogo para sort de tabla de proveedores

--- webapp/controller/Home.controller.js
+++ webapp/controller/Home.controller.js
@@ -2,24 +2,37 @@ sap.ui.define([
     "sap/ui/core/mvc/Controller",
     "com/bootcamp/sapui5/providers/utils/HomeHelper",
     "sap/ui/model/Filter",
-    "sap/ui/model/FilterOperator"
-], (Controller, HomeHelper, Filter, FilterOperator) => {
+    "sap/ui/model/FilterOperator",
+    "sap/ui/model/Sorter",
+    "sap/ui/core/Fragment",
+], (Controller, HomeHelper, Filter, FilterOperator, Sorter, Fragment) => {
     "use strict";
 
     return Controller.extend("com.bootcamp.sapui5.providers.controller.Home", {
         onInit() {
             this.oRouter = this.getOwnerComponent().getRouter();
+
+            this._mViewSettingsDialogs = {}; // Objeto para almacenar los diálogos
+
+            Fragment.load({
+                id: this.getView().getId(),
+                name: "com.bootcamp.sapui5.providers.view.fragments.SortDialog", 
+                controller: this
+            }).then(function (oDialog) {
+                this._mViewSettingsDialogs["SortDialog"] = oDialog;
+                this.getView().addDependent(oDialog);
+            }.bind(this));
         },
 
-        onPress: async function(){
+        onPress: async function () {
             let oFilter = [];
             let values = this.getOwnerComponent().getModel("LocalDataModel").getData()
             console.log(values)
-            
-            if(values.valueInput){
+
+            if (values.valueInput) {
                 oFilter.push(new Filter("SupplierID", FilterOperator.EQ, values.valueInput));
             }
-            if(values.selectedKey){
+            if (values.selectedKey) {
                 oFilter.push(new Filter("CompanyName", FilterOperator.EQ, values.selectedKey));
             }
             let oDatos = await HomeHelper.getDataProviders(oFilter);
@@ -27,13 +40,32 @@ sap.ui.define([
         },
 
 
-        onItemPress: function(oEvent){
+        onItemPress: function (oEvent) {
             let oSource = oEvent.getSource();
             let oDatos = oSource.getBindingContext("ProviderCollection").getObject();
             this.oRouter.navTo("detail", {
-            SupplierID: oDatos.SupplierID
+                SupplierID: oDatos.SupplierID
             });
+        },
+        handleSortButtonPressed: function () {
+            let oDialog = this._mViewSettingsDialogs["SortDialog"];
+            if (oDialog) {
+                oDialog.open();
             }
-            
+        },
+
+        handleSortDialogConfirm: function (oEvent) {
+            let oTable = this.byId("SuppliersTable"), 
+                mParams = oEvent.getParameters(),
+                oBinding = oTable.getBinding("items"),
+                aSorters = [];
+        
+            let sPath = mParams.sortItem.getKey();
+            let bDescending = mParams.sortDescending;
+            aSorters.push(new sap.ui.model.Sorter(sPath, bDescending));
+        
+            oBinding.sort(aSorters);
+        }
+        
     });
 });
